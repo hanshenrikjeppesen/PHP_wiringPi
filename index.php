b988283774587201877a6c9a5ee2d2fa1ab9c7ac
hanshenrikjeppesen <info@frist.dk>
2018-03-18 23:48:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>RaspberryPi - learning PHP</title>
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<style>
+input[type="submit"]{
+  display:block;
+  margin: 30px auto;
+  border-radius: 21px;
+  border: none;
+  padding: 10px;
+  width: 60%;
+  
+  cursor:pointer;
+  text-transform:uppercase;
+  transition:0.5s all ease-in-out;
+
+ }
+input[type="submit"]:hover{
+  letter-spacing:5px;
+  font-weight:bold;
+  
+}
+.form{
+  background: linear-gradient(182deg, #a8e3f2, #3d8fb8, #136f9c);
+  background-size: 600% 600%;
+
+  -webkit-animation: AnimationName 30s ease infinite;
+  -moz-animation: AnimationName 30s ease infinite;
+  animation: AnimationName 30s ease infinite;
+
+  @-webkit-keyframes AnimationName {
+    0%{background-position:0% 30%}
+    50%{background-position:100% 71%}
+    100%{background-position:0% 30%}
+  }
+  @-moz-keyframes AnimationName {
+    0%{background-position:0% 30%}
+    50%{background-position:100% 71%}
+    100%{background-position:0% 30%}
+  }
+  @keyframes AnimationName {
+    0%{background-position:0% 30%}
+    50%{background-position:100% 71%}
+    100%{background-position:0% 30%}
+  }
+}
+	</style>
 </head>
 <body>
 	<div class="topbox">
 	<h1>Controlling GPIO from PHP</h1>
 	</div>
-	
+<div class="form">	
 <form method="get" action="index.php">
 		<input type="submit" value="ON" name="on">
               <input type="submit" value="OFF" name="off">
-         </form>
+	</form></div>
          <?php
          $setmode17 = shell_exec("/usr/local/bin/gpio -g mode 5 out");
          if(isset($_GET['on'])){
